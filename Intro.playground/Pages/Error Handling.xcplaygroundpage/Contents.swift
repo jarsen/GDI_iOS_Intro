6a461f7d98388488541bbeb264dc2834fc238bc2
Jason Larsen <jason.larsen@gmail.com>
2015-07-11 08:47:35 -0600

    here we go!

--- Intro.playground/Pages/Error Handling.xcplaygroundpage/Contents.swift
+++ Intro.playground/Pages/Error Handling.xcplaygroundpage/Contents.swift
@@ -2,6 +2,32 @@
 
 import Foundation
 
-var str = "Hello, playground"
+/*:
+# Error Handling
+Swift 2 introduced error handling. Functions that use the keyword `throws` before the return type must handled inside of a `do-catch` block.
+*/
+
+enum PizzaError : ErrorType {
+    case InvalidPizza
+}
+
+func makePizza(name: String) throws -> Void {
+    switch name {
+    case "Pepperoni", "Cheese", "Margherita":
+        return
+    default:
+        throw PizzaError.InvalidPizza
+    }
+}
+
+do {
+    try makePizza("Cheese")
+}
+catch PizzaError.InvalidPizza {
+    print("Unknown pizza!")
+}
+catch {
+    print("Unknown error: \(error)")
+}
 
 //: [Next](@next)
--- Intro.playground/Pages/Protocols.xcplaygroundpage/Contents.swift
+++ Intro.playground/Pages/Protocols.xcplaygroundpage/Contents.swift
@@ -2,6 +2,40 @@
 
 import Foundation
 
-var str = "Hello, playground"
+/*:
+# Protocols
+
+Protocols are lists of methods and variables. Any object that _conforms_ to a protocol must implement all of the required methods and variables.
+
+Whenever possible, you should prefer this to subclassing, because it is more flexible.
+*/
+
+protocol Employee {
+    var name: String { get }
+    var age: Int { get }
+    var hourly: Double { get }
+    
+    func wage() -> Double
+}
+
+//: Notice we have multiple protocols
+
+struct DeliveryGirl : Employee, CustomStringConvertible {
+    var name: String
+    var age: Int
+    var hourly: Double
+    
+    var tips = 0.0
+    
+    var description: String {
+        get {
+            return "\(name) is \(age) and makes \(wage())"
+        }
+    }
+    
+    func wage() -> Double {
+        return hourly * 8 + tips
+    }
+}
 
 //: [Next](@next)
